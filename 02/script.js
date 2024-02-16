7925c0c0e2ef9c1220eb1f6b90fd4c5696f40525
MUKUL <mukul.2226csit1115@kiet.edu>
2024-02-16 15:39:25 +0530

    Variables

---
+++ 02/script.js
@@ -0,0 +1,48 @@
+console.log("Live reload Enabled")
+
+ //Var 
+// DataType varName = value ; Strongly Typed Lan 
+//Js --> Loosely typed Lang , explicitly make variables
+//  var age = 22;
+//  age = "Piyush "
+//  console.log(age) 
+
+
+ //Naming Convention 
+ //firstname
+ //first_name
+//FirstName
+ //⭐firstName
+
+
+ //var --> Gobal Scope 
+//  if(true){
+//     var age = 23;
+//     console.log(age)
+//  }
+
+//  console.log(number );
+
+//  var number = 10;
+//  console.log(number )
+
+
+
+//let variable 
+// let age = 22;
+//   age = "piyush"
+//   console.log(age);
+
+//   if(true ){
+//     let a = 20;
+//       console.log(a)
+//   }
+//   console.log(a)
+
+
+//Const 
+//Scope -> Local 
+//Value reassign nhi ho sakti
+//Also CONST KE CASE me valuye undefined nhi kar sakte ..ie aise nhi hai phle variable declare kar diya and baadnme khi par use value de rhe ho 
+let age = "22"
+console.log(age )
