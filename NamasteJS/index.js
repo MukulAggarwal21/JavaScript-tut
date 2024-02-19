77c5ac8ef8138d30668d923236a0c47d92459bb6
MUKUL <mukul.2226csit1115@kiet.edu>
2024-02-19 16:39:58 +0530

    CallBack Function

--- NamasteJS/index.js
+++ NamasteJS/index.js
@@ -1,5 +1,21 @@
-{
-    var a =10;
-    let b =20;
-    const c =30;
-}
+// {
+//     var a =10;
+//     let b =20;
+//     const c =30;
+// }
+
+
+
+//CALLBACK FUNCTION 
+setTimeout( function(){
+ console.log("TIMER")
+}, 5000);
+ function x(y)
+ {
+    console.log("x");
+    y();
+ }
+
+ x( function y(){
+    console.log("y");
+ });
