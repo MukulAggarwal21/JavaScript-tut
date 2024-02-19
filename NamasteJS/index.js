b6565cbed4b5e23a69d93a623d8d5368be040be7
MUKUL <mukul.2226csit1115@kiet.edu>
2024-02-19 17:58:33 +0530

    Event Listeners

--- NamasteJS/index.js
+++ NamasteJS/index.js
@@ -7,15 +7,30 @@
 
 
 //CALLBACK FUNCTION 
-setTimeout( function(){
- console.log("TIMER")
-}, 5000);
- function x(y)
- {
-    console.log("x");
-    y();
- }
+// setTimeout( function(){
+//  console.log("TIMER")
+// }, 5000);
+//  function x(y)
+//  {
+//     console.log("x");
+//     y();
+//  }
 
- x( function y(){
-    console.log("y");
+//  x( function y(){
+//     console.log("y");
+//  });
+
+
+
+
+//EventListeners
+
+   function attachEventListners(){
+      let count = 0;
+ document.getElementById("ClickMe")
+ .addEventListener("click" , function xyz(){
+   console.log("Button Clicked " , count++);
  });
+   }
+
+attachEventListners();
