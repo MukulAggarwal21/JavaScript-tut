6a6d71eae9acce2faeb47296373984148b79e395
MUKUL <mukul.2226csit1115@kiet.edu>
2024-02-21 00:17:06 +0530

    Optimes code (functional Programing)

--- NamasteJS/index.js
+++ NamasteJS/index.js
@@ -46,39 +46,60 @@
 //Here Y is Higher Order Function
 
 
-const radius = [3,1,2,4];
-
-const calculateArea = function(radius){
-  const output = [];
-  for(let i=0 ; i<radius.length ; i++){
-    output.push(Math.PI * radius[i]*radius[i]);
-  }
-  return output;
-}
-console.log(calculateArea(radius));
-
-
-const calculateCircumference = function (radius){
-  const output = [];
-   for(let i=0 ; i<radius.length ; i++){
-    output.push(2*Math.PI *radius[i]);
-  }
-  return output;
-}
-console.log(calculateCircumference(radius));
-
-const calculateDiameter = function (radius){
-  const output = [];
-   for(let i=0 ; i<radius.length ; i++){
-    output.push(2*radius[i]);
-  }
-  return output;
-}
-console.log(calculateDiameter(radius));
-
-
-//Optimised Code 
+//Functional programming
 // const radius = [3,1,2,4];
-// const area = function(radius){
-//    return Math.PI * radius[i]*radius[i];
+
+// const calculateArea = function(radius){
+//   const output = [];
+//   for(let i=0 ; i<radius.length ; i++){
+//     output.push(Math.PI * radius[i]*radius[i]);
+//   }
+//   return output;
 // }
+// console.log(calculateArea(radius));
+
+
+// const calculateCircumference = function (radius){
+//   const output = [];
+//    for(let i=0 ; i<radius.length ; i++){
+//     output.push(2*Math.PI *radius[i]);
+//   }
+//   return output;
+// }
+// console.log(calculateCircumference(radius));
+
+// const calculateDiameter = function (radius){
+//   const output = [];
+//    for(let i=0 ; i<radius.length ; i++){
+//     output.push(2*radius[i]);
+//   }
+//   return output;
+// }
+// console.log(calculateDiameter(radius));
+
+
+
+
+// Optimised Code 
+const radius = [3,1,2,4];
+const area = function(radius){
+   return Math.PI * radius*radius;
+}
+const Circumference = function(radius){
+  return  2*Math.PI.radius;
+}
+const diameter = function(radius){
+  return 2*radius
+}
+
+const Calculate = function(radius){
+ const output = [];
+  for(let i=0 ; i<radius.length ; i++){
+    output.push(logic(radius[i]));
+  }
+  return output;
+}
+console.log(calculate(radius,area));
+console.log(calculate(radius,circumference));
+console.log(calculate(radius,diameter))
+
